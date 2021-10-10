9fce5108eccc440cab94e266aae6943ecc8fa941
zydane07 <muhammadzydane3@gmail.com>
2021-10-11 00:30:11 +0700

    navbar kelar

--- index.php
+++ index.php
@@ -7,42 +7,53 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <title>Dashboard</title>
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light bg-white nav-font my-shadow pt-3 pb-3">
         <div class="container">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <a class="navbar-brand pe-5" href="#">Navbar w/ text</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Beranda</a>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item pe-3">
+                        <a class="nav-link active fw-bold nav__link" aria-current="page" href="#">Beranda</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item pe-3">
                         <a class="nav-link" href="#">Bantuan</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item pe-3">
                         <a class="nav-link" href="#">Tentang Aplikasi</a>
                     </li>
-                    <div class="d-flex justify-content-end">
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Mohamad Zydane Ulir Rizqi Toyyibi
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                    </div>
-
+                </ul>
+                <ul class="navbar-nav mb-2 mb-lg-0">
+                    <li class="nav-item d-flex align-items-center">
+                        <div class="text-center">
+                            <img src="icons/Bell.svg" class="pe-3" alt="notifikasi">
+                        </div>
+                    </li>
+                    <li class="nav-item d-flex align-items-center">
+                        <div class="text-center">
+                            <img src="img/profil 1.png" class="rounded nav-pict" alt="photo profil">
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown ps-1">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Mohamad Zydane Ulir Rizqi Toyyibi
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item" href="#">Profile</a></li>
+                            <li><a class="dropdown-item" href="#">Event Saya</a></li>
+                            <li><a class="dropdown-item" href="#">Lihat Sertifikat</a></li>
+                            <li><a class="dropdown-item" href="#">Keluar</a></li>
+                        </ul>
+                    </li>
                 </ul>
             </div>
         </div>
