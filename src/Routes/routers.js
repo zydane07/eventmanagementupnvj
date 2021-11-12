15717dcb932bbb1971ae325e9691f40319d3a969
bagasss3 <64892035+bagasss3@users.noreply.github.com>
2021-11-12 22:19:40 +0700

    Update routers.js

--- src/Routes/routers.js
+++ src/Routes/routers.js
@@ -53,6 +53,28 @@ routers.get('/profile',Auth,(req,res)=>{
     ...obj
   });
 })
-
 */
+const homePage = require('../Controller/getHomepage');
+routers.get('/',homePage);
+
+const postHomePage = require('../Controller/homepage');
+routers.post('/',postHomePage);
+
+const detailEvent = require ('../Controller/getDetailEvent');
+routers.get('/detail/:id_event', detailEvent);
+
+// const daftarEvent = require ('../Controller/daftarEvent');
+// routers.post('/detail/:id_event', daftarEvent);
+
+const myEvent = require('../Controller/getMyEvent');
+routers.get('/profile/event', myEvent);
+
+// const mySertif = require('../Controller/getMySertif');
+// routers.get('/sertifikat', mySertif);
+
+const logoutAcc = require('../Controller/logoutAcc');
+routers.delete('/logout', logoutAcc);
+
+// const detailEvent = require('../Controller/getDetailEvent');
+// routers.get('/detail',detailEvent);
 module.exports = routers
