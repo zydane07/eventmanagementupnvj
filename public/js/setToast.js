6f6ba96c31f8c4e034899ee481ade94c33449ad8
zydane07 <muhammadzydane3@gmail.com>
2021-12-05 22:21:32 +0700

    toastr

--- public/js/setToast.js
+++ public/js/setToast.js
@@ -1,11 +1,11 @@
 $("#bantuan, #ttg, #pengenalan").on("click", function () {
     toastr.error("Fitur masih dalam tahap pengembangan", "Warning");
-    var audio = new Audio("sounds/notification.mp3");
+    var audio = new Audio("/sounds/notification.mp3");
     audio.play();
 });
 
 $("#addwish, #daftar-event").on("click", function () {
     toastr.success("Event Berhasil ditambahkan ke Wishlist", "Success");
-    var audio = new Audio("sounds/notification.mp3");
+    var audio = new Audio("/sounds/notification.mp3");
     audio.play();
 });
--- src/Models/mahasiswa.js
+++ src/Models/mahasiswa.js
@@ -1,48 +1,48 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const savedEvent = mongoose.Schema({
-  id_event: String
+    id_event: String,
 });
 
 const historyEvent = mongoose.Schema({
-  id_event: String,
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+    id_event: String,
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
 });
 
 const mahasiswaSchema = mongoose.Schema({
-  email: String,
-  nama_lengkap: String,
-  password: String,
-  no_hp: String,
-  jenis_kelamin: String,
-  tanggal_lahir: Date,
-  nim: String,
-  fakultas: String,
-  prodi: String,
-  angkatan: String,
-  photo: {
-    avatar: { 
-      type:  String,
-      default: 'https://res.cloudinary.com/dz1q2dbty/image/upload/v1637411143/ifpbpwuf5yrtlgbexgf1.png'
+    email: String,
+    nama_lengkap: String,
+    password: String,
+    no_hp: String,
+    jenis_kelamin: String,
+    tanggal_lahir: Date,
+    nim: String,
+    fakultas: String,
+    prodi: String,
+    angkatan: String,
+    photo: {
+        avatar: {
+            type: String,
+            default: "https://res.cloudinary.com/dz1q2dbty/image/upload/v1637467460/nxpkjd0hoqt4cekpg9xa.png",
+        },
+        cloudinary_id: {
+            type: String,
+            default: "nxpkjd0hoqt4cekpg9xa",
+        },
+        path: {
+            type: String,
+            default: "/img/PP/DefaultPhoto.png",
+        },
     },
-    cloudinary_id: {
-      type: String,
-      default: 'ifpbpwuf5yrtlgbexgf1'
+    isVerified: Boolean,
+    savedEvent: {
+        type: [savedEvent],
+        require: false,
     },
-    path: {
-      type: String,
-      default: '/img/PP/pp.png'
-    }
-  },
-  isVerified: Boolean,
-  savedEvent: {
-    type:[savedEvent],
-    require: false
-  },
-  historyEvent: [historyEvent]
+    historyEvent: [historyEvent],
 });
 
-module.exports = mongoose.model('Mahasiswa', mahasiswaSchema);
+module.exports = mongoose.model("Mahasiswa", mahasiswaSchema);
