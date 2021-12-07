de1743fab2e61ebffd28b4875357a37720229858
Bagas Prabaswara <bagasprabaswara77@gmail.com>
2021-12-07 12:27:36 +0700

    test2 commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 const routers = require('./src/routes');
 const cspMiddleware = require('./src/csp');
