9230b64229dcbe3dba0dc088913f2a32ad011ca0
Bagas Prabaswara <bagasprabaswara77@gmail.com>
2021-12-11 08:17:50 +0700

    fix email using googleapis

--- src/Nodemailer/sendEmail.js
+++ src/Nodemailer/sendEmail.js
@@ -1,21 +1,24 @@
 const nodemailer = require('nodemailer');
-
+const { google } = require('googleapis');
 // eslint-disable-next-line consistent-return
 module.exports = async (email, subject, text) => {
+  const oAuth2Client = new google.auth.OAuth2(process.env.CLIENT_ID,process.env.CLIENT_SECRET,process.env.REDIRECT_URI);
+  oAuth2Client.setCredentials({refresh_token: process.env.REFRESH_TOKEN});
   try {
+    const accessToken = await oAuth2Client.getAccessToken();
     const transporter = nodemailer.createTransport({
-      service: 'Gmail',
+      service: 'gmail',
       auth: {
+        type: 'OAuth2',
         user: process.env.GMAIL,
-        pass: process.env.PASS_GMAIL,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken: process.env.REFRESH_TOKEN,
+        accessToken: accessToken
       },
-      tls: {
-        rejectUnauthorized: false,
-      },
-
     });
     await transporter.sendMail({
-      from:process.env.GMAIL,
+      from:'SIM-U',
       to:email,
       subject,
       generateTextFromHTML: true,
